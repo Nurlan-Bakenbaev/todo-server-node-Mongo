f76842a1d5269e4fd9b08c70fad0c17367e515d8
Nurlan <iamsamurai1991@gmail.com>
2023-10-19 22:45:08 +0200

    new file:   .history/index_20231019224505.js 	new file:   .history/index_20231019224507.js 	modified:   index.js

--- .history/index_20231019224505.js
+++ .history/index_20231019224505.js
@@ -7,11 +7,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://frolicking-malasada-801d2a.netlify.app/",
-  })
-);
+app.use(cors({
+  origin: 'https://frolicking-malasada-801d2a.netlify.app/',
+}));
 app.use("/api/auth", authRouter);
 app.use("/api/todo", todoRouter);
 
